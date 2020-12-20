70f2e7e912ac975f48820e30dbce30d72b184b10
Terri Perigo <tperigo3@gmail.com>
2020-12-19 21:41:38 -0600

    finish the support classes

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -15,6 +15,11 @@ namespace TechJobsOO
 
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
 
+        public Location(string value) : this()
+        {
+            Value = value;
+        }
+
         public override bool Equals(object obj)
         {
             return obj is Location location &&
